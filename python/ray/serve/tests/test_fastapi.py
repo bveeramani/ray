2c81bc02ac6e2b165ba33a32ee313fdc9086804c
Balaji Veeramani <bveeramani@berkeley.edu>
2021-12-31 21:52:02 -0800

    Appease flake8

--- python/ray/serve/tests/test_fastapi.py
+++ python/ray/serve/tests/test_fastapi.py
@@ -581,8 +581,8 @@ def test_fastapi_method_redefinition(serve_instance):
         def method(self):
             return "hi get"
 
-        @app.post("/")  # noqa: F811 method redefinition
-        def method(self):
+        @app.post("/")
+        def method(self):  # noqa: F811 method redefinition
             return "hi post"
 
     A.deploy()
